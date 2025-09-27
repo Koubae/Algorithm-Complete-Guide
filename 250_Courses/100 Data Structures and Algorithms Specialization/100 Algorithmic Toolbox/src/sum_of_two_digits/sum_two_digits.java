63fbc8b7e1fa1b618a0391e052b432ed0f22f3a9
koubae <58447627+Koubae@users.noreply.github.com>
2025-09-27 06:44:28 +0200

    Sync changes

--- 250_Courses/100 Data Structures and Algorithms Specialization/100 Algorithmic Toolbox/src/sum_of_two_digits/sum_two_digits.java
+++ 250_Courses/100 Data Structures and Algorithms Specialization/100 Algorithmic Toolbox/src/sum_of_two_digits/sum_two_digits.java
@@ -1,8 +1,15 @@
-
+import java.util.Scanner;
 
 class SumTwoDigits {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
 
-        System.out.println("Hell world")
+        System.out.println(sumTwoDigits(a, b));
+    }
+
+    static int sumTwoDigits(int a, int b) {
+        return a + b;
     }
 }
